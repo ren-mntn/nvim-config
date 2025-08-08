9584cf28f5f4955a8e3ce0b1fee3c62dc822b9f6
ren-mntn <r.otter.camp+git@gmail.com>
2025-08-08 23:13:50 +0900

    tsgoを使う設定

--- lua/plugins/lsp_settings.lua
+++ lua/plugins/lsp_settings.lua
@@ -1,10 +1,14 @@
 return {
+  -- LSP設定
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" }, -- ファイル読み込み時
     opts = {
       servers = {
         typos_lsp = {},
+        -- tsgoは2025年末までLSP未対応のため、vtslsを使用
+        tsserver = false, -- 古いtsserverは無効化
+        -- vtsls = {},  -- LazyVimのTypeScript extrasで自動有効化されるのでコメントアウト
       },
     },
   },
