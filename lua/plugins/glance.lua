2fb3a8a195b99811b56dec390423fa4c37ecb52f
ren-mntn <r.otter.camp+git@gmail.com>
2025-08-23 11:41:59 +0900

    glance追加

---
+++ lua/plugins/glance.lua
@@ -0,0 +1,19 @@
+--[[
+機能概要: LSPの定義/参照/実装をVSCode風のピークプレビューで表示するプラグイン
+設定内容: 最小設定でglance.nvimを導入、基本的なキーマッピングを設定
+キーバインド: gD(定義), gR(参照), gY(型定義), gM(実装)
+--]]
+return {
+  "DNLHC/glance.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "gD", "<cmd>Glance definitions<cr>", desc = "定義を表示" },
+    { "gR", "<cmd>Glance references<cr>", desc = "参照を表示" },
+    { "gY", "<cmd>Glance type_definitions<cr>", desc = "型定義を表示" },
+    { "gM", "<cmd>Glance implementations<cr>", desc = "実装を表示" },
+  },
+  opts = {
+    height = 18,
+    zindex = 45,
+  },
+}
